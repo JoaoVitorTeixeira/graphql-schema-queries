54ea009d7ac5975b98f02a3d70ea8d03c6afb3bf
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2021-10-26 09:50:20 -0300

    feat: add user status as enum

--- data/db.js
+++ data/db.js
@@ -1,40 +1,43 @@
 const users = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'johndoe@email.com',
-        age: 32,
-        profileId: 1
-    },
-    {
-        id: 2,
-        name: 'Jane Doe',
-        email: 'janeDoe@email.com',
-        age: 28,
-        realSalary: 1000.45,
-        profileId: 2
-    },
-    {
-        id: 3,
-        name: 'John Smith',
-        email: 'jonhSmith@email.com',
-        age: 32,
-        profileId: 1,
-    }
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@email.com",
+    age: 32,
+    profileId: 1,
+    status: "ACTIVE",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    email: "janeDoe@email.com",
+    age: 28,
+    realSalary: 1000.45,
+    profileId: 2,
+    status: "INACTIVE",
+  },
+  {
+    id: 3,
+    name: "John Smith",
+    email: "jonhSmith@email.com",
+    age: 32,
+    profileId: 1,
+    status: "BLOCKED",
+  },
 ];
 
 const profiles = [
-    {
-        id: 1,
-        name: 'Admin',
-    },
-    {
-        id: 2,
-        name: 'Common',
-    }
+  {
+    id: 1,
+    name: "Admin",
+  },
+  {
+    id: 2,
+    name: "Common",
+  },
 ];
 
 module.exports = {
-    users,
-    profiles
+  users,
+  profiles,
 };
